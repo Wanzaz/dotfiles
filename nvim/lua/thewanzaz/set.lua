1070839bf93bf206de6fee372fe7be56b7cfe617
Wanzaz <ondra.pazourek@gmail.com>
2023-02-09 11:51:14 +0100

    Fix HTML tag colors

--- nvim/lua/thewanzaz/set.lua
+++ nvim/lua/thewanzaz/set.lua
@@ -29,3 +29,6 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+-- au BufRead,BufNewFile *.html set syntax=html
+vim.cmd 'autocmd BufEnter *.html set syntax=html'

b90f18ea1f3308fa84078851ddb21eb048bc2a92
Wanzaz <ondra.pazourek@gmail.com>
2023-04-02 16:36:18 +0200

    Change from 4 files - uiop to 3 files iop

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,4 +1,7 @@
 local builtin = require('telescope.builtin')
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<C-u>", function() ui.nav_file(1) end)
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-n>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()

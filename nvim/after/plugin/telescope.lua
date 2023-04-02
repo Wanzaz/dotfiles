169e06597edbf43c0966d536aaa1f74984040416
Wanzaz <ondra.pazourek@gmail.com>
2023-04-02 16:50:58 +0200

    Comment unnecessary part of the code

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,9 @@
 local builtin = require('telescope.builtin')
-local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<C-u>", function() ui.nav_file(1) end)
+-- COMMENT EXPLANATION: to have only 3 files iop - to have the Ctrl + U func
+-- local ui = require("harpoon.ui")
+
+-- vim.keymap.set("n", "<C-u>", function() ui.nav_file(1) end)
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-n>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
